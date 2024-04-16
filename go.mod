cbaf0acdc816d7b02a601cc7ad87e651657f7d83
schmidtkesandro <sandroluis@terra.com.br>
2024-04-16 09:32:49 -0300

    inclusão do arquivo env

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.3
 
 require (
 	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/time v0.5.0
 )
--- rate-limiter_test.go
+++ rate-limiter_test.go
@@ -4,74 +4,77 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"golang.org/x/time/rate"
 )
 
-func TestRateLimiter_Middleware(t *testing.T) {
-	// Criar uma instância do RateLimiter
-	limiter := NewRateLimiter(DefaultConfig)
+func TestRateLimiterMiddleware(t *testing.T) {
+	// Cria um servidor HTTP de teste com o middleware do Rate Limiter
+	limiter := createTestRateLimiter()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(limiter.Middleware)
 
-	// Criar um handler de teste
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Test response"))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
 	})
-
-	// Criar um request de teste com IP e token
+	//t.Errorf("-1-------------------------------")
+	// Teste para requisição dentro do limite
 	req := httptest.NewRequest("GET", "/", nil)
-	req.RemoteAddr = "192.168.1.1:12345"
-	req.Header.Set("API_KEY", "test_token")
-
-	// Criar um ResponseWriter de teste
-	rr := httptest.NewRecorder()
-
-	// Chamar o middleware
-	limiter.Middleware(handler).ServeHTTP(rr, req)
-
-	// Verificar o código de status
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler retornou código de status errado: esperado %v, recebido %v", http.StatusOK, status)
+	req.RemoteAddr = "192.168.1.1:1234" // IP válido
+	req.Header.Set("API_KEY", "")       // Token inválido
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("1 Request should be allowed but got status code %d e %d", resp.Code, http.StatusOK)
 	}
-
-	// Verificar o corpo da resposta
-	expected := "Test response"
-	if rr.Body.String() != expected {
-		t.Errorf("Handler retornou corpo de resposta errado: esperado %v, recebido %v", expected, rr.Body.String())
+	//t.Errorf("-2----------------------------")
+	// Teste para requisição excedendo o limite de IP
+	resp1 := http.StatusOK
+	for i := 0; i < 20; i++ {
+		resp = httptest.NewRecorder()
+		r.ServeHTTP(resp, req)
+		resp1 = resp.Code
+	}
+	if resp1 != http.StatusTooManyRequests {
+		t.Errorf("2 Request should be blocked due to IP rate limit but got status code %d e %d", resp.Code, http.StatusTooManyRequests)
+	}
+	//t.Errorf("-3-------------------------------")
+	// Teste para requisição excedendo o limite de token
+	req.RemoteAddr = "192.168.1.0:1234"      // IP válido
+	req.Header.Set("API_KEY", "valid_token") // Token válido
+	resp = httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("3 Request should be blocked due to token rate limit but got status code %d e %d", resp.Code, http.StatusTooManyRequests)
+	}
+	// Teste para requisição excedendo o limite de Token
+	resp1 = http.StatusOK
+	for i := 0; i < 20; i++ {
+		resp = httptest.NewRecorder()
+		r.ServeHTTP(resp, req)
+		resp1 = resp.Code
+	}
+	if resp1 != http.StatusTooManyRequests {
+		t.Errorf("4 Request should be blocked due to token rate limit but got status code %d e %d", resp.Code, http.StatusTooManyRequests)
 	}
 }
 
-func TestRateLimiter_Integration(t *testing.T) {
-	// Configurar o RateLimiter
-	limiter := NewRateLimiter(DefaultConfig)
-	limiter.SetTokenRateLimit("test_token", DefaultConfig)
-
-	// Iniciar o servidor HTTP de teste
-	server := httptest.NewServer(limiter.Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Test response"))
-	})))
-	defer server.Close()
-
-	// Criar um client HTTP para enviar solicitações para o servidor de teste
-	client := &http.Client{}
-
-	// Enviar várias solicitações para testar os limites
-	for i := 0; i < 105; i++ {
-		req, err := http.NewRequest("GET", server.URL, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		req.Header.Set("API_KEY", "test_token")
-		_, err = client.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Verificar se o servidor respondeu conforme o esperado
-	resp, err := client.Get(server.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusTooManyRequests {
-		t.Errorf("Esperado status de erro 429, recebido %d", resp.StatusCode)
+// createTestRateLimiter cria um RateLimiter com configurações para teste
+func createTestRateLimiter() *RateLimiter {
+	return &RateLimiter{
+		Config: Config{
+			MaxRequests:      10,              // Limite de requisições por segundo
+			IPBlockPeriod:    1 * time.Minute, // Período de bloqueio para IPs
+			TokenBlockPeriod: 1 * time.Minute, // Período de bloqueio para tokens
+		},
+		IPRateLimiters:  make(map[string]*rate.Limiter),
+		TokenRateLimits: make(map[string]TokenRateLimit),
+		IPBlockList:     make(map[string]time.Time),
+		TokenBlockList:  make(map[string]time.Time),
+		TokenConfigs:    make(map[string]Config),
 	}
 }
